a73b473bb5cce314a257ed0013c316f8e8fe7744
manicos <manicos@gmail.com>
2020-03-20 09:54:22 -0600

    remove setings

--- DjangoMariaDB/app/apirest/settings.py
+++ DjangoMariaDB/app/apirest/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp',
-    'rest_framework',
-    "rest_framework_api_key",
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
