9eda74279f57dee0250a05f75264a155975931a6
manicos <manicos@gmail.com>
2020-05-15 19:47:35 -0500

    update cert

--- DjangoMariaDB/create_cert.sh
+++ DjangoMariaDB/create_cert.sh
@@ -1,1 +1,1 @@
-docker-compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot --email admin_dti@pikit.mx  -d claseweb.pikit.mx --rsa-key-size 4096 --agree-tos  --force-renewal" certbot
+docker-compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot --email admin_dti@pikit.mx  -d claseweb2.pikit.mx --rsa-key-size 4096 --agree-tos  --force-renewal" certbot
